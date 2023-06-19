99f90ebbe38cb2852f073881c8aff0b1d46bd1b2
rajeshm27 <rcmutyala@gmail.com>
2023-06-18 21:33:57 -0500

    changes to gitignore file

---
+++ terraform/S3.tf
@@ -0,0 +1,77 @@
+# resource "aws_s3_bucket" "rajesh-source-bucket" {
+#     bucket = "rajesh-source-bucket"
+#     #region =var.aws_region
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# } 
+
+# resource "aws_s3_bucket" "rajesh-raw-bucket" {
+#     bucket = "rajesh-raw-bucket"
+#     #region =var.aws_region
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# } 
+
+# resource "aws_s3_bucket" "rajesh-code-bucket" {
+#     bucket = "rajesh-code-bucket"
+#     #region =var.aws_region
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
+
+# resource "aws_s3_object" "object" {
+#   bucket = "rajesh-code-bucket"
+#   key    = "movielens/config/config.json"
+#   #acl = "private"
+#   source = "../src/config/config.json"
+
+#   etag = filemd5("../src/config/config.json")
+# }
+
+# resource "aws_s3_object" "process_json" {
+#   bucket = "rajesh-code-bucket"
+#   key    = "movielens/config/process_config.json"
+#   #acl = "private"
+#   source = "../src/config/process_config.json"
+
+#   etag = filemd5("../src/config/process_config.json")
+# }
+
+
+
+resource "aws_s3_bucket" "rajesh-source-tf" {
+  bucket = "rajesh-source-tf"
+}
+
+resource "aws_s3_bucket" "rajesh-raw-tf" {
+  bucket = "rajesh-raw-tf"
+}
+
+resource "aws_s3_bucket" "rajesh-code-tf" {
+  bucket = "rajesh-code-tf"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.rajesh-code-tf.id
+  key    = "movielens/config/config.json"
+  acl    = "private"
+  source = "/Users/rajeshmutyala/Documents/Projects/data_ingestion_project/src/config/config.json"
+  etag   = filemd5("/Users/rajeshmutyala/Documents/Projects/data_ingestion_project/src/config/config.json")
+}
+
+resource "aws_s3_object" "upload_objects" {
+  for_each = local.config_files
+
+  bucket = aws_s3_bucket.rajesh-source-tf.id
+  key    = "/Users/rajeshmutyala/Documents/Projects/data_ingestion_project/movielens_dataset/${each.value}"
+  source = "/Users/rajeshmutyala/Documents/Projects/data_ingestion_project/movielens_dataset/${each.value}"
+
+  etag = filemd5("/Users/rajeshmutyala/Documents/Projects/data_ingestion_project/movielens_dataset/${each.value}")
+}
+
+locals {
+config_files = fileset("/Users/rajeshmutyala/Documents/Projects/data_ingestion_projectmovielens_dataset/", "*/.csv")
+}
